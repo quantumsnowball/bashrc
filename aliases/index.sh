c3b5dba93e8959086c1c49548a42266c59b11a27
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-10 16:46:08 +0800

    rft; eza alias

--- aliases/index.sh
+++ aliases/index.sh
@@ -9,18 +9,11 @@ alias cl=clear
 alias rl='exec bash'
 # exit
 alias x=exit
-# eza as ls
-alias ls='eza -F --icons'
-alias  l='eza -F --icons'
-alias la='eza -aF --icons'
-alias lt='eza -aF -s time --reverse --icons'
-alias ll='eza -aF -l -h --icons --grid'
-alias lT='eza -aF -l -h --icons --tree --level=2'
-alias zz='z -'
 # process
 alias jobs='jobs -l'
 
 
+source $alias_root/eza.sh
 source $alias_root/dir.sh
 source $alias_root/nvim.sh
 source $alias_root/youtube-dl.sh
