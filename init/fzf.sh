622c884c5edadab6551844ccfb277a9420aa9bd9
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-10 15:22:56 +0800

    rft

---
+++ init/fzf.sh
@@ -0,0 +1,4 @@
+# fzf
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+export FZF_DEFAULT_OPTS='--border --layout=reverse --height=50% --min-height=20'
+
--- init/index.sh
+++ init/index.sh
@@ -1,10 +1,6 @@
 init_root=$HOME/.config/bashrc/init
 
 source $init_root/env.sh
+source $init_root/fzf.sh
+source $init_root/zoxide.sh
 
-# zoxide
-eval "$(zoxide init bash)"
-
-# fzf
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
-export FZF_DEFAULT_OPTS='--border --layout=reverse --height=50% --min-height=20'
