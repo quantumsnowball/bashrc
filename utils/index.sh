ee9f50cb0c390b057d82ce8df953e6c7f38a08e0
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-10 16:38:43 +0800

    rft: utils dir

--- utils/index.sh
+++ utils/index.sh
@@ -1,12 +1,4 @@
-# mkdir and then cd into it in one command
-mkcd ()
-{
-    mkdir -p -- "$1" && cd -P -- "$1" 
-}
+utils_root=$HOME/.config/bashrc/utils
 
-# create a file with all intermediate directories
-onetouch()
-{
-    mkdir -p $(dirname $1) && touch $1;
-}
+source $utils_root/dir.sh
 
