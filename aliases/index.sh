9ea250e22dab336ddd4fd88a70a3db41d7a4ee1c
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-10 16:46:55 +0800

    rft: shell alias

--- aliases/index.sh
+++ aliases/index.sh
@@ -1,18 +1,7 @@
 alias_root=$HOME/.config/bashrc/aliases
 
-# sudo
-alias sudo='sudo '
-# clear
-alias c=clear
-alias cl=clear
-# reload shell
-alias rl='exec bash'
-# exit
-alias x=exit
-# process
-alias jobs='jobs -l'
 
-
+source $alias_root/shell.sh
 source $alias_root/eza.sh
 source $alias_root/dir.sh
 source $alias_root/nvim.sh
---
+++ aliases/shell.sh
@@ -0,0 +1,13 @@
+# sudo
+alias sudo='sudo '
+# clear
+alias c=clear
+alias cl=clear
+# reload shell
+alias rl='exec bash'
+# exit
+alias x=exit
+# process
+alias jobs='jobs -l'
+
+
